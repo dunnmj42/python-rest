5c70222a9c4f60b2a37375f83c3872d4ee462465
Michael Dunn <dunnmj42@gmail.com>
2021-02-24 09:16:07 -0600

    GET

--- hello.py
+++ hello.py
@@ -1,5 +1,19 @@
-from flask import Flask
+import flask
+from flask import request, jsonify
+
 app = Flask(__name__)
+app.config["DEBUG"] = True
+
+pets = [
+    {
+        'name': 'Miso',
+        'owner': 'Mike'
+    },
+    {
+        'name': 'Tuna',
+        'owner': 'Liza'
+    }
+]
 
 @app.route('/')
 def index():
@@ -8,3 +22,9 @@ def index():
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+  
+@app.route('/api/pets/all', methods=['GET'])
+def api_all():
+    return jsonify(pets)
+  
+app.run()
